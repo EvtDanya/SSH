dbadf5390f482eebfed765ab172f9ef5ce94c4b9
EvtDanya <fomindanjj@rambler.ru>
2023-07-14 23:41:07 +0700

    added ssh client to work with ssh servers

--- ssh_cmd.py
+++ ssh_cmd.py
@@ -8,20 +8,13 @@ def create_ssh_connection(ip, port, user, passwd):
     client.connect(ip, port=port, username=user, password=passwd)
     return client
 
-def execute_commands(client, user):
+def execute_command(client, cmd):
     ssh_session = client.get_transport().open_session()
     if ssh_session.active:
-        while True:
-            cmd = input(f'{user}$ ')
-            if not cmd:
-                break
-            elif cmd.lower() == 'exit':
-                break
-            ssh_session.exec_command(cmd)
-            output = ssh_session.recv(1024).decode('utf-8')
-            print(output, end='')
-    client.close()
-    
+        ssh_session.exec_command(cmd)
+        output = ssh_session.recv(1024).decode('utf-8')
+        print(output, end='')
+
 if __name__ ==  '__main__':
     user = input('Username: ')
     passwd = getpass.getpass()
@@ -30,4 +23,13 @@ if __name__ ==  '__main__':
     port = int(input('Enter server port: ') or 22)
     
     client = create_ssh_connection(ip, port, user, passwd)
-    execute_commands(client, user)
+    
+    while True:
+        cmd = input(f'{user}$ ')
+        if not cmd:
+            break
+        elif cmd.lower() == 'exit':
+            break
+        execute_command(client, cmd)
+
+    client.close()
